02718435e8ca9ed8bd0555de4d091449da6cf65a
CynthiaBudiono <c14180210@john.petra.ac.id>
2022-06-14 00:14:29 +0800

    db

--- database/toko_online.sql
+++ database/toko_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: sql309.epizy.com
--- Generation Time: Jun 13, 2022 at 12:49 PM
+-- Generation Time: Jun 13, 2022 at 01:13 PM
 -- Server version: 10.3.27-MariaDB
 -- PHP Version: 7.2.22
 
@@ -66,7 +66,8 @@ CREATE TABLE `order` (
 INSERT INTO `order` (`id`, `id_customer`, `nama_pemesanan`, `alamat_pengiriman`, `jumlah_pembayaran`, `bukti_pembayaran`, `keterangan`, `created`) VALUES
 (1, 1, 'budii', '                                                                        jln sini2                                                               ', 535000, 'pembayaran-1-20220612001732.png', 'tolong kirim secepatnya', '2022-06-11 17:17:32'),
 (2, 1, 'admin', 'jln jln', 484000, '', '-', '2022-06-11 16:07:33'),
-(3, 2, 'cynthia', '                                                                        jln haha 122, kodepos 67665                                                                ', 470000, 'pembayaran-3-20220613223731.jpg', 'hoodie spektakulernya warna hitam L', '2022-06-13 15:35:24');
+(3, 2, 'cynthia', '                                                                        jln haha 122, kodepos 67665                                                                ', 470000, 'pembayaran-3-20220613223731.jpg', 'hoodie spektakulernya warna hitam L', '2022-06-13 15:35:24'),
+(4, 3, 'dummy', 'jln dummy 11, JAKSEL', 295000, '', '-', '2022-06-13 15:49:41');
 
 -- --------------------------------------------------------
 
@@ -91,7 +92,8 @@ INSERT INTO `order_detail` (`id`, `id_order`, `id_produk`, `jumlah`) VALUES
 (3, 2, 3, 1),
 (4, 2, 2, 1),
 (5, 3, 5, 1),
-(6, 3, 1, 1);
+(6, 3, 1, 1),
+(7, 4, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +119,7 @@ CREATE TABLE `produk` (
 
 INSERT INTO `produk` (`id`, `nama`, `harga`, `foto`, `stok`, `status`, `keterangan`, `created`, `updated`) VALUES
 (1, 'Hoodie IM Possible', 240000, 'Produk-1-20220611105310.jpg', 100, 1, '                                    Sudah menggunakan bahan Fleece, tidak terlalu tebal dan juga tidak terlalu tipis sehingga sangat cocok dan nyaman digunakan di iklim tropis Indonesia                                ', '2022-06-11 01:13:04', '2022-06-11 02:53:10'),
-(2, 'Kaos MerryForTees', 295000, 'Produk-2-20220611104350.jpg', 326, 1, 'Cotton, Denim                                ', '2022-06-11 01:20:00', '2022-06-11 02:43:50'),
+(2, 'Kaos MerryForTees', 295000, 'Produk-2-20220611104350.jpg', 325, 1, 'Cotton, Denim                                ', '2022-06-11 01:20:00', '2022-06-11 02:43:50'),
 (3, 'PRIMERRY 2 TEES CROWN', 189000, 'Produk-3-20220611134107.jpg', 0, 1, '                               Round Neck                                     ', '2022-06-11 05:41:07', '2022-06-11 06:41:07'),
 (4, 'PRIMERRY 2 TEES STRIPS', 295000, 'Produk-4-20220612002211.jpg', 5, 1, '                                    hitam                                                              ', '2022-06-11 16:22:11', '2022-06-11 16:22:21'),
 (5, 'Hoodie IM POSSIBLE Spektakuler Merry Riana', 230000, 'Produk-5-20220613220744.jpg', 884, 1, 'Bahan dasar FLEECE yang berkualitas (adem, halus) serta dengan jahitan yang rapi                                       ', '2022-06-14 02:07:44', '2022-06-13 15:05:36');
@@ -147,7 +149,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`, `nama`, `tipe`, `email`, `no_hp`, `alamat`, `created`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'customer', 'xx@gmail.com', '081112', 'jln jln', '2022-06-10 13:10:35'),
 (2, 'cboo', '21232f297a57a5a743894a0e4a801fc3', 'cynthia', 'customer', 'xx@gmail.com', '', '', '2022-06-12 01:46:05'),
-(3, 'dummy', '202cb962ac59075b964b07152d234b70', 'dummy', 'customer', 'dummy@gmail.com', '', '', '2022-06-14 02:38:29');
+(3, 'dummy', '202cb962ac59075b964b07152d234b70', 'dummy', 'customer', 'dummy@gmail.com', '0812121212', 'jln dummy 11, JAKSEL', '2022-06-14 02:38:29');
 
 --
 -- Indexes for dumped tables
@@ -197,13 +199,13 @@ ALTER TABLE `general`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `produk`
